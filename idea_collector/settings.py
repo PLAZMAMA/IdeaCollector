6c3323afff9013bf63d148388123c24955fb888e
PLAZMAMA <maorioriori@gmail.com>
2021-02-14 16:10:26 -0500

    added celery to installed apps and started adding celery variables

--- idea_collector/settings.py
+++ idea_collector/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
     'rest_framework',
     'idea'
 ]
@@ -129,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#celery configuration
+CELERY_RESULT_BACKEND = 'django-db'
