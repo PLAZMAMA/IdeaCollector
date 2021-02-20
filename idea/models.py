f2a3acac530da6a720df4c22aaa27cd49dc7abcc
PLAZMAMA <maorioriori@gmail.com>
2021-02-19 23:26:23 -0500

    possible fix for trying to add date_time with auto_now_add=True without a default

--- idea/models.py
+++ idea/models.py
@@ -5,7 +5,7 @@ class IdeaModel(models.Model):
     title = models.CharField(max_length=128, null=True)
     description = models.TextField(max_length=512, null=True)
     picture = models.ImageField(upload_to=f'images', null=True)
-    date_time = models.DateTimeField(auto_now_add=True)
+    date_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return f'{self.pk}, {self.title}, {self.description}, {self.picture}'
