6956acab34b5af14e4f7839132abb404c8425635
PLAZMAMA <maorioriori@gmail.com>
2021-02-19 22:38:12 -0500

    finished get_most_recent_ideas task

--- idea/tasks.py
+++ idea/tasks.py
@@ -1,6 +1,17 @@
 from celery import shared_task
 from idea.models import IdeaModel
+from channels.layers import get_channel_layer
+from time import sleep
+
+channel_layer = get_channel_layer()
 
 @shared_task
-def show_ideas():
-    IdeaModel.objects.all()
+def get_most_recent_ideas(num_of_ideas, most_recent_ideas):
+    """gets the most recent "num_of_ideas""""
+    last_most_recent_ideas = most_recent_ideas
+    while(last_most_recent_ideas != most_recent_ideas):
+        most_recent_ideas = IdeaModel.objects.all().order_by('-date')[:num_of_ideas:-1]
+        sleep(3)
+    
+    return(most_recent_ideas)
+    
