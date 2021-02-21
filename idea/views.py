bd07859989f3bf8209360e9fee91062aed9904f1
PLAZMAMA <maorioriori@gmail.com>
2021-02-21 02:17:15 -0500

    made the view change dynamicaly from test to deployment

--- idea/views.py
+++ idea/views.py
@@ -2,10 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 from idea.models import IdeaModel
 from idea.serializers import IdeaSerializer
 from idea_collector.celery import app
+from idea_collector.settings import DEBUG
 
 class IdeaViewSet(ModelViewSet):
     queryset = IdeaModel.objects.all()
     serializer_class = IdeaSerializer
     def create(self, request, *args, **kwargs):
-        app.send_task('tasks.get_most_recent_ideas')
+        if not(DEBUG):
+            app.send_task('tasks.get_most_recent_ideas')
+            
         return super().create(request, *args, **kwargs)
