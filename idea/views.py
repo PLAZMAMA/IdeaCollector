22f810fb9bf69ce87ef2457c41f1bead2c4ee4a0
PLAZMAMA <maorioriori@gmail.com>
2021-02-20 22:50:15 -0500

    possible solution for accomplishing the websocket GetMostRecentIdeas by every time that a new idea gets added it sends a task to a celery worker which then calls a function from the GetMostRecentIdeas class

--- idea/views.py
+++ idea/views.py
@@ -1,7 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from idea.models import IdeaModel
 from idea.serializers import IdeaSerializer
+from idea_collector.celery import app
 
 class IdeaViewSet(ModelViewSet):
     queryset = IdeaModel.objects.all()
     serializer_class = IdeaSerializer
+    def create(self, request, *args, **kwargs):
+        app.send_task('tasks.get_most_recent_ideas')
+        return super().create(request, *args, **kwargs)
