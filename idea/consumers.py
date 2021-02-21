e0a637f2446fcc2e8dd91e1afb8cdc3b92aea37c
PLAZMAMA <maorioriori@gmail.com>
2021-02-21 02:18:23 -0500

    deleted a function and changed the others occurdinly

--- idea/consumers.py
+++ idea/consumers.py
@@ -5,14 +5,9 @@ class GetMostRecent(AsyncWebsockerConsumer):
     async def connect(self):
         await self.channel_layer.group_add('most_recent', self.channel_name)
         await self.accept()
-        await self.get_most_recent()
-        
 
     async def disconnect(self):
         await self.channel_layer.group_discard('most_recent', self.channel_name)
-
-    async def get_most_recent(self):
-
 
     async def send_most_recent(self, event):
         ideas = [[idea.title, idea.description] for idea in event['text']]
