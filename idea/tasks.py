b68c7ac71b71a9412acf98383c56c95bb2a524da
PLAZMAMA <maorioriori@gmail.com>
2021-02-21 11:04:24 -0500

    cleaned and specified publish_most_recent_ideas task

--- idea/tasks.py
+++ idea/tasks.py
@@ -7,12 +7,9 @@ from time import sleep
 channel_layer = get_channel_layer()
 
 @shared_task
-def get_most_recent_ideas(num_of_ideas, most_recent_ideas):
+def publish_most_recent_ideas(num_of_ideas):
     """gets the most recent "num_of_ideas" ideas"""
-    last_most_recent_ideas = most_recent_ideas
-    while(last_most_recent_ideas != most_recent_ideas):
-        most_recent_ideas = IdeaModel.objects.all().order_by('-date_time')[:num_of_ideas]
-        sleep(3)
+    most_recent_ideas = IdeaModel.objects.all().order_by('-date_time')[:num_of_ideas]
 
     try:
         async_to_sync(channel_layer.group_send)('most_recent', {'type': 'get_most_recent','text': most_recent_ideas})
