d90cfd2517e668eb2bc56967c99d7923af5019ca
PLAZMAMA <maorioriori@gmail.com>
2021-02-20 22:43:45 -0500

    changed method name from get_most_recent to send_most_recent for better description of what the method does

--- idea/consumers.py
+++ idea/consumers.py
@@ -1,14 +1,20 @@
 from channels.generic.websocket import AsyncWebsockerConsumer
+from idea_collector.celery import app
 
 class GetMostRecent(AsyncWebsockerConsumer):
     async def connect(self):
         await self.channel_layer.group_add('most_recent', self.channel_name)
         await self.accept()
+        await self.get_most_recent()
+        
 
     async def disconnect(self):
         await self.channel_layer.group_discard('most_recent', self.channel_name)
 
-    async def get_most_recent(self, event):
+    async def get_most_recent(self):
+
+
+    async def send_most_recent(self, event):
         ideas = [[idea.title, idea.description] for idea in event['text']]
         await self.send(ideas)
         
