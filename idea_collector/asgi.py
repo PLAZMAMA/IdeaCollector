c086f4e64288c84565546c0ec184142dc07faca7
PLAZMAMA <maorioriori@gmail.com>
2021-02-24 00:11:56 -0500

    possible fix for apps not loaded yet thrown by django when running daphne

--- idea_collector/asgi.py
+++ idea_collector/asgi.py
@@ -8,13 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+from idea_collector.routing import ws_urlpatterns
+from django.core.asgi import get_asgi_application
+#calling the asgi app so the following imports work
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack, URLRouter
-from idea_collector.routing import ws_urlpatterns
 
-django_asgi_app = get_asgi_application()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'idea_collector.settings')
 
 application = ProtocolTypeRouter({
