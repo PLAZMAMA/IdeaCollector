fea561255a8e28159ebf78ab2783e4be957242c6
PLAZMAMA <maorioriori@gmail.com>
2021-02-23 23:32:40 -0500

    possible fix for apps not loaded yet raised by Django

--- idea_collector/asgi.py
+++ idea_collector/asgi.py
@@ -11,12 +11,13 @@ import os
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
-#from channels.auth import AuthMiddlewareStack, URLRouter
+from channels.auth import AuthMiddlewareStack, URLRouter
 from idea_collector.routing import ws_urlpatterns
 
+django_asgi_app = get_asgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'idea_collector.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    #'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
 })
