0cbf2b4f0e9ba526b733bd71ff2c90f6fb2c9bf8
PLAZMAMA <maorioriori@gmail.com>
2021-02-24 19:44:55 -0500

    modified the task to retry after 5 sec if failed and retry at max 3 times

--- idea/tasks.py
+++ idea/tasks.py
@@ -6,7 +6,7 @@ from time import sleep
 
 channel_layer = get_channel_layer()
 
-@shared_task(bind=True)
+@shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 3, 'countdown': 5})
 def publish_most_recent_ideas(self, num_of_ideas=5):
     """gets the most recent "num_of_ideas" ideas"""
     most_recent_ideas = IdeaModel.objects.all().order_by('-date_time')[:num_of_ideas]
@@ -14,4 +14,4 @@ def publish_most_recent_ideas(self, num_of_ideas=5):
     try:
         async_to_sync(channel_layer.group_send)('most_recent', {'type': 'get_most_recent','text': most_recent_ideas})
     except Exception as e:
-        raise self.retry(f'no one is connected to the websocket connection but just in case, the function will run again(retry): {e}')
+        raise Exception(f'no one is connected to the websocket connection but just in case, the function will run again(retry) in 5 seconds: {e}', countdown=5)
