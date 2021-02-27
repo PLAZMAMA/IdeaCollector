6bfdd7023f9ad81124f4f22b138137a62b030c60
PLAZMAMA <maorioriori@gmail.com>
2021-02-27 00:57:44 -0500

    changed the old entries in the ws_urlpatterns and added two new ones that use the django channels workers

--- idea_collector/routing.py
+++ idea_collector/routing.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from idea.consumers import GetMostRecent, GetRandomIdea
+from idea.consumers import GetMostRecentIdea, GetRandomIdea, GetRandomIdeaChannels, GetMostRecentIdeasChannels
 
 ws_urlpatterns = [
-    path('ws/most_recent_ideas/', GetMostRecent.as_asgi()),
-    path('ws/get_random_idea/', GetRandomIdea.as_asgi())
+    path('ws/most_recent_ideas/', GetMostRecentIdea.as_asgi()),
+    path('ws/get_random_idea/', GetRandomIdea.as_asgi()),
+    path('ws/channels/most_recent_ideas/', GetRandomIdeaChannels.as_asgi()),
+    path('ws/channels/get_random_idea/', GetMostRecentIdeasChannels.as_asgi()),
 ]
