1cc59fa33c49d42c0f579fbc73fe6c12b3a8f8a7
PLAZMAMA <maorioriori@gmail.com>
2021-02-27 00:47:59 -0500

    fixed some typos and the exception thats raised in publish most recent ideas

--- idea/tasks.py
+++ idea/tasks.py
@@ -14,13 +14,14 @@ def publish_most_recent_ideas(self, num_of_ideas=5):
     queryset = IdeaModel.objects.all().order_by('-date_time')[:num_of_ideas]
     most_recent_ideas = IdeaSerializer(queryset, many=True)
     try:
-        async_to_sync(channel_layer.group_send)('most_recent', {'type': 'get_most_recent', 'text': most_recent_ideas})
+        async_to_sync(channel_layer.group_send)('most_recent', {'type': 'send_most_recent', 'text': most_recent_ideas})
 
     except Exception as e:
-        raise Exception(f'no one is connected to the websocket connection but just in case, the function will run again(retry) in 5 seconds: {e}', countdown=5)
+        raise Exception(f'no one is connected to the websocket connection but just in case, the function will run again(retry) in 5 seconds: {e}')
 
 @shared_task
 def get_random_idea():
+    """get a random idea"""
     queryset = IdeaModel.objects.all()
     random_idea = IdeaSerializer(queryset[randint(0, len(queryset)-1)])
-    async_to_sync(channel_layer.group_send)('random_idea', {'type': 'get_random_idea', 'text': random_idea})
+    async_to_sync(channel_layer.group_send)('random_idea', {'type': 'send_random_idea', 'text': random_idea})
