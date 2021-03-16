d4424349929f21d61943a3adbedcee109d1b4f99
PLAZMAMA <maorioriori@gmail.com>
2021-03-15 20:56:16 -0400

    added new static file config

--- idea_collector/urls.py
+++ idea_collector/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.WEBSOCKET_URL, document_root=settings.WEBSOCKET_ROOT)
