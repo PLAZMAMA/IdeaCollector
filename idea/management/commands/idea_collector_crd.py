a1b6aa9088b49554d3f56dc12fcc4e37d51f1e62
PLAZMAMA <maorioriori@gmail.com>
2021-03-22 10:01:42 -0400

    fixed some bugs and changed the verbose argument's defualt

--- idea/management/commands/idea_collector_crd.py
+++ idea/management/commands/idea_collector_crd.py
@@ -3,13 +3,15 @@ from idea_collector.settings import BASE_DIR
 import os
 
 class Command(BaseCommand):
+    help = 'creates a new idea_collector operator'
+
     def add_arguments(self, parser):
-        parser.add_arguments('-v', '--verbose', type=bool, help='boolean of whether to add the verbose argument to the kopf operator or not(default: False)', default=False)
+        parser.add_argument('-verb', '--verbose', type=bool, help='boolean of whether to add the verbose argument to the kopf operator or not(default: False)', default=True)
 
     def handle(self, *args, **options):
         try:
-            crd_path = os.join.path(BASE_DIR, 'operator/idea-collector-crd')
-            kopf_path = os.join.path(BASE_DIR, 'operator/idea_collector_operator')
+            crd_path = os.path.join(BASE_DIR, 'operator/idea-collector-crd.yaml')
+            kopf_path = os.path.join(BASE_DIR, 'operator/idea_collector_operator.py')
             os.system(f'kubectl apply -f {crd_path}')
             if options['verbose']:
                 os.system(f'pipenv run kopf run --verbose {kopf_path}')
@@ -18,4 +20,4 @@ class Command(BaseCommand):
                 os.system(f'pipenv run kopf run {kopf_path}')
         
         except Exception as e:
-            raise Exception(f'pipenv or kubectl are not installed properly: {e}')
+            raise Exception(f'pipenv, kubectl or minikube are not installed properly: {e}')
